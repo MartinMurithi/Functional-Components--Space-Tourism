3a3c10332bbe6191cd6b9f9729c2b41ce0abd9a0
MartinMurithi <wachiramartin442@gmail.com>
2023-02-22 01:08:25 +0300

    Functional Components

---
+++ src/Components/Main.jsx
@@ -0,0 +1,25 @@
+import "../Components/Main.css";
+import React from "react";
+
+function Main() {
+  return (
+    <div className="gridContainer">
+      <div className="intro">
+        <p className="text1">SO, YOU WANT TO TRAVEL TO </p>
+        <p className="text2">Space</p>
+        <p className="text3">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience! Explore
+        </p>
+      </div>
+
+          <div className="exploreDiv">
+              <p className="explore">EXPLORE</p>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
